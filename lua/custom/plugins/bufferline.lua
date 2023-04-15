22056991a4a52cbe00822269f6b2bd74a7ed7ee3
droidthis <hasifresearch@gmail.com>
2023-04-15 17:04:22 +0800

    nvim personal dev env

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,25 @@
+return {
+
+{
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+  },
+  opts = {
+    options = {
+     always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+}
+
+}
