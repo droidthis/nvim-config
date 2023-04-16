54392b95620599e4703a43b1519441f02c8c62c9
droidthis <hasifresearch@gmail.com>
2023-04-16 08:34:03 +0800

    codeium test

--- init.lua
+++ init.lua
@@ -234,12 +234,12 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
-vim.keymap.set( 'i', '<S><Tab>', function ()
-  require("copilot.panel").accept()
-
-end
-)
+--
+-- vim.keymap.set( 'i', '<S><Tab>', function ()
+--   require("copilot.panel").accept()
+--
+-- end
+-- )
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
